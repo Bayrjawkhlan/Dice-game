32f8994bb82c2032312e4ab2a83ec78a76d78abb
Bayarjavkhlan <bayrjawkhlanb@gmail.com>
2022-10-25 10:42:51 +0800

    added by callBack function

--- dice-game/app.js
+++ dice-game/app.js
@@ -12,18 +12,19 @@ var roundScore = 0;
 
 // Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
 
-var dice = Math.floor(Math.random() * 6) + 1;
-
-// <div class="player-score" id="score-0">43</div>
-
-// window.document.querySelector('#score-0').textContent = dice
-// document.querySelector('#score-1').textContent = dice
+var diceNumber = Math.floor(Math.random() * 6) + 1;
 
 // Програм эхлэхэд бэлтгэе
-document.querySelector('#score-0').textContent = 0;
-document.querySelector('#score-1').textContent = 0;
-document.querySelector('#current-0').textContent = 0;
-document.querySelector('#current-1').textContent = 0;
-document.querySelector('.dice').style.display = "none";
 
-console.log('Шоо :' + dice);
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.dice').style.display = "none";
+document.querySelector('.btn-roll').addEventListener('click', rollTheDice);
+
+function rollTheDice() {
+	var diceNumber = Math.floor(Math.random() * 6) + 1;
+	alert('Шоо буулаа : ' + diceNumber);
+}
