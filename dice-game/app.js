9517c6ab812b40591efa9eddcbbeee022ba35c20
Bayarjavkhlan <bayrjawkhlanb@gmail.com>
2022-11-15 13:58:41 +0800

    remove space

--- dice-game/app.js
+++ dice-game/app.js
@@ -15,7 +15,7 @@ var diceNumber = Math.floor(Math.random() * 6) + 1;
 document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
 document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0'; 
+document.getElementById('current-1').textContent = '0';
 
 var diceDom = document.querySelector('.dice')
 diceDom.style.display = "none";
